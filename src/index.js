c875781edfb562421c26c6968f4c61a01a450a7c
nazarisabbot <n.updesign@gmail.com>
2024-03-14 19:35:59 +0600

    ninth step R7

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import comparisonFiles from './comparisonFiles.js';
 import formatter from './formatters/index.js';
 
-const runComparisonFiles = (path1, path2, options) => {
+const runComparisonFiles = (path1, path2, options = {}) => {
   const result = comparisonFiles(path1, path2);
   const { format } = options;
   const formattedDiff = formatter(result, format);
